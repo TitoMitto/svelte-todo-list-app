e0bab87b48151d495ff9ea5fb41d515166b8322b
Tito Mitto <mordeccai9@gmail.com>
2022-11-01 20:06:16 +0300

    todo list app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script type="ts">
+    let userInput = "";
+    let todoItems: string[] = [];
+
+    function addToList(e: Event){
+        e.preventDefault();
+        todoItems = [...todoItems, userInput];
+    }
+
+    function removeItem(index: number){
+        todoItems.splice(index, 1);
+        todoItems = [...todoItems];
+    }
+</script>
+
+
+<h3> Tasks </h3>
+
+{#if todoItems.length > 0}
+You have { todoItems.length } task(s)
+{:else}
+No tasks added
+<br>
+{/if}
+
+{#each todoItems as todoItem, index}
+
+    <p> { index + 1}. {todoItem}  <button class="danger" on:click={()=>removeItem(index)}> X </button> </p>
+{/each}
+
+<form on:submit={addToList}>
+    <input type="text" bind:value={userInput}>
+    <button type="submit">Add Todo</button>
+</form>
+
+<style>
+    .danger {
+        color: red;
+        cursor: pointer;
+    }
+</style>
